246c23fbf07e05cd1d3acd156f93fb1c36f22fc7
Taras Piont <taras.piont@gmail.com>
2020-11-29 20:37:33 +0100

    added some bootstrap fitures and javascript

--- Tweet4Tar/tweets/serializers.py
+++ Tweet4Tar/tweets/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from .models import Tweet
+import random
 
 
 
 
 class TweetListSerializer(serializers.ModelSerializer):
-
+    likes = serializers.SerializerMethodField('get_likes')
     class Meta:
         model = Tweet
-        fields = ['id','content']
+        fields = ['id','content','likes']
+    
+    def get_likes(self,request):
+        return random.randint(0,9999)
+
+
+    
